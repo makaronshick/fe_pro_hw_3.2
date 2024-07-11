6abe9206da35076759be0b9a1826a5bd2273b655
makaronshick <makaronshick@ukr.net>
2024-07-11 17:21:15 +0300

    'throw' deleted. default values added

--- script.js
+++ script.js
@@ -3,49 +3,49 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 
-const userName = prompt('Enter your name');
+let userName = prompt('Enter your name');
 
 if (userName === null) {
-    alert("You clicked 'Cancel'");
-    throw '';
+    alert("You clicked 'Cancel'. Default value 'User' saved");
+    userName = 'User';
 }
 else if (!userName.trim()) {
-    alert('You entered nothing (or spaces only)');
-    throw '';
+    alert("You entered nothing (or spaces only). Default value 'User' saved");
+    userName = 'User';
 }
 else if (!isNaN(userName)) {
-    alert('You entered numbers');
-    throw '';
+    alert("You entered numbers. Default value 'User' saved");
+    userName = 'User';
 }
 
-const userCity = prompt('Enter your city');
+let userCity = prompt('Enter your city');
 
 if (userCity === null) {
-    alert("You clicked 'Cancel'");
-    throw '';
+    alert("You clicked 'Cancel'. Default value 'City' saved");
+    userCity = 'City';
 }
-else if (!userCity.trim()) {
-    alert('You entered nothing (or spaces only)');
-    throw '';
+else if (!serCity.trim()) {
+    alert("You entered nothing (or spaces only). Default value 'City' saved");
+    userCity = 'City';
 }
 else if (!isNaN(userCity)) {
-    alert('You entered numbers');
-    throw '';
+    alert("You entered numbers. Default value 'City' saved");
+    userCity = 'City';
 }
 
-const userEyeColor = prompt('Enter your eyes color');
+let userEyeColor = prompt('Enter your eyes color');
 
 if (userEyeColor === null) {
-    alert("You clicked 'Cancel'");
-    throw '';
+    alert("You clicked 'Cancel'. Default value 'Color' saved");
+    userEyeColor = 'Color';
 }
 else if (!userEyeColor.trim()) {
-    alert('You entered nothing (or spaces only)');
-    throw '';
+    alert("You entered nothing (or spaces only). Default value 'Color' saved");
+    userEyeColor = 'Color';
 }
 else if (!isNaN(userEyeColor)) {
-    alert('You entered numbers');
-    throw '';
+    alert("You entered numbers. Default value 'Color' saved");
+    userEyeColor = 'Color';
 }
 
 alert(`Hi, ${userName} from ${userCity}! You have beautiful ${userEyeColor} eyes!`);
